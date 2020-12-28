cc983f21600183f5120a53143a13adc3488fd255
Drongo-1 <ngugidavid46@gmail.com>
2020-12-28 13:04:21 +0000

    added the alert effect

--- js/script.js
+++ js/script.js
@@ -10,11 +10,83 @@ $(document).ready(function(){
     $(".imp3,.show3").click(function(){
         $("div.imp3").toggle();
         $("div.show3").toggle();
-    })
-    $(".poico1").mouseover(function(){
-        $(".pi1").show(function(){
-            $(".lur1").blur();
-        });
-    }); 
+    }) 
+    $(function(){
+        $(".lur1").mouseover(function(){
+            $(".poico1a").show();
+            $(this).css("color","white")
 
+        }).mouseleave(function(){
+            $(".poico1a").hide();
+        })
+    })
+    $(function(){
+        $(".lur2").mouseover(function(){
+            $(".poico2a").show();
+            $(this).css("color","white")
+
+        }).mouseleave(function(){
+            $(".poico2a").hide();
+        })
+    })
+    $(function(){
+        $(".lur3").mouseover(function(){
+            $(".poico3a").show();
+            $(this).css("color","white")
+
+        }).mouseleave(function(){
+            $(".poico3a").hide();
+        })
+    })
+    $(function(){
+        $(".lur4").mouseover(function(){
+            $(".poico4a").show();
+            $(this).css("color","white")
+
+        }).mouseleave(function(){
+            $(".poico4a").hide();
+        })
+    })
+    $(function(){
+        $(".lur5").mouseover(function(){
+            $(".poico5a").show();
+            $(this).css("color","white")
+
+        }).mouseleave(function(){
+            $(".poico5a").hide();
+        })
+    })
+    $(function(){
+        $(".lur6").mouseover(function(){
+            $(".poico6a").show();
+            $(this).css("color","white")
+
+        }).mouseleave(function(){
+            $(".poico6a").hide();
+        })
+    })
+    $(function(){
+        $(".lur7").mouseover(function(){
+            $(".poico7a").show();
+            $(this).css("color","white")
+
+        }).mouseleave(function(){
+            $(".poico7a").hide();
+        })
+    })
+    $(function(){
+        $(".lur8").mouseover(function(){
+            $(".poico8a").show();
+            $(this).css("color","white")
+
+        }).mouseleave(function(){
+            $(".poico8a").hide();
+        })
+    })
+    
 });
+function alat(){
+    // event.preventDefault();
+    var cal = document.getElementById("name").value;
+    alert("Dear "+cal+", we have received your message. Thank you fro reaching out to us" );
+}
